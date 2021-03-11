473546dea083d445db31c044b4ef94b24679301b
joelmeiller <joel@oscillate.ch>
2021-03-11 17:50:54 +0100

    correct typescript build errors

--- src/theme/util.ts
+++ src/theme/util.ts
@@ -1,5 +1,12 @@
 import { FormTheme } from './theme'
 import { getUseInlineStyle, getUseCSSStyles } from 'frr-web/lib/theme/util'
+import { CSSProperties } from 'frr-web/lib/theme/theme'
 
 export const useInlineStyle = getUseInlineStyle<FormTheme>()
-export const useCSSStyles = getUseCSSStyles<FormTheme>()
+export const useCSSStyles: <C extends keyof FormTheme>(
+  theme: FormTheme,
+  componentKey: C,
+) => (override?: Partial<FormTheme[C]>) => <K extends keyof FormTheme[C]>(
+  elementKeys: Array<K> | K,
+  internalOverride?: CSSProperties,
+) => string = getUseCSSStyles<FormTheme>()
