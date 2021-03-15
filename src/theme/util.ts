da791c85bdd0a0e36914d3e4accbf234df169450
joelmeiller <joel@oscillate.ch>
2021-03-15 09:30:46 +0100

    fix util types

--- src/theme/util.ts
+++ src/theme/util.ts
@@ -1,5 +1,24 @@
 import { FormTheme } from './theme'
 import { getUseInlineStyle, getUseCSSStyles } from 'frr-web/lib/theme/util'
+import { CSSProperties } from 'styled-components'
 
-export const useInlineStyle = getUseInlineStyle<FormTheme>()
-export const useCSSStyles = getUseCSSStyles<FormTheme>()
+export const useInlineStyle: <C extends keyof FormTheme>(
+  theme: FormTheme,
+  componentKey: C,
+) => (
+  override?: Partial<FormTheme[C]>,
+) => <K extends keyof FormTheme[C]>(
+  elementKeys: Array<K> | K,
+  internalOverride?: CSSProperties,
+  className?: string,
+) => { style: FormTheme[C][K]; id: string } = getUseInlineStyle<FormTheme>()
+
+export const useCSSStyles: <C extends keyof FormTheme>(
+  theme: FormTheme,
+  componentKey: C,
+) => (
+  override?: Partial<FormTheme[C]>,
+) => <K extends keyof FormTheme[C]>(
+  elementKeys: Array<K> | K,
+  internalOverride?: CSSProperties,
+) => { cssStyles: string; id: string } = getUseCSSStyles<FormTheme>()
